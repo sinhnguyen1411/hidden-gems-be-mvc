2a9f6aa629d4e078512f0146caef6083fe889deb
sinhnguyen1411 <ng.dms1411@gmail.com>
2025-08-20 14:02:55 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,17 +13,17 @@ class User
         return $row ?: null;
     }
 
-    public static function create(string $name, string $email, string $passwordHash, string $role='customer'): int
+    public static function create(string $ten_dang_nhap, string $email, string $mat_khau_ma_hoa, string $vai_tro='user', string $ho_va_ten=null, string $so_dien_thoai=null): int
     {
-        $stmt = DB::pdo()->prepare('INSERT INTO users(name,email,password_hash,role) VALUES(?,?,?,?)');
-        $stmt->execute([$name,$email,$passwordHash,$role]);
+        $stmt = DB::pdo()->prepare('INSERT INTO users(ten_dang_nhap, email, mat_khau_ma_hoa, vai_tro, ho_va_ten, so_dien_thoai) VALUES(?,?,?,?,?,?)');
+        $stmt->execute([$ten_dang_nhap, $email, $mat_khau_ma_hoa, $vai_tro, $ho_va_ten, $so_dien_thoai]);
         return (int)DB::pdo()->lastInsertId();
     }
 
-    public static function saveRefreshToken(int $id, string $token): void
+    public static function saveRefreshToken(int $id_user, string $token): void
     {
-        $stmt = DB::pdo()->prepare('UPDATE users SET refresh_token=? WHERE id=?');
-        $stmt->execute([$token,$id]);
+        $stmt = DB::pdo()->prepare('UPDATE users SET refresh_token=? WHERE id_user=?');
+        $stmt->execute([$token,$id_user]);
     }
 
     public static function findByRefreshToken(string $token): ?array
@@ -36,7 +36,7 @@ class User
 
     public static function all(): array
     {
-        $stmt = DB::pdo()->query('SELECT id,name,email,role FROM users');
+        $stmt = DB::pdo()->query('SELECT id_user, ten_dang_nhap, email, ho_va_ten, vai_tro, so_dien_thoai, ngay_tham_gia FROM users');
         return $stmt->fetchAll();
     }
 }
